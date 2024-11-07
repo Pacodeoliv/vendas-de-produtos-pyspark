8d016a1602cd00b49d6bfee7a096972cd12dd7ee
Paco <pacodeoliveirasaavedra@gmail.com>
2024-11-07 18:18:13 -0300

    pyspark editado, DF feito , testando comandos de withColumn agora

--- pyspark-basico.py
+++ pyspark-basico.py
@@ -26,15 +26,44 @@ df = spark.read.option("header", True).csv(file_path)
 # df.show()
 
 
-df_limitando = df.limit(10) #limita o dataframe do pyspark para 10 linhas
+df_limitando = df.limit(20) #limita o dataframe do pyspark para 10 linhas
 
-limitado1 = df_limitando.toPandas() #converte o df limitado pra pandas
+limitado1 = df_limitando.toPandas() #converte o df limitado pra pandas   (só funciona na 3.11 ou menor)
 
+customers = dl = spark.read.option("header", True).csv(file_path)
 
-# rows = df.limit(10).collect() #coleta os dados como uma lista de Row
-
-
-st.dataframe(limitado1)
+customers.printSchema()
 
 
+#st.dataframe(limitado1)  # imprime o DF no streamlits local
+
+#st.dataframe(customers.select("customer_id", "customer_state").limit(10)) # faz um select e imprime em baixo do de cima
+#st.dataframe(customers.select(F.countDistinct(F.col("customer_id")).alias("count_distinct_cutomer_id")).limit(10)) # 
+#st.dataframe(customers.filter(F.col("customer_state") == "SP").limit(10))
+
+
+
+################################################################################################
+
+file_path2 = os.path.expanduser("~/workshop/vendas-de-produtos-pyspark/tables/olist_orders_dataset.csv")
+
+
+orders = spark.read.option("header", True).csv(file_path2)
+
+
+#st.dataframe(orders.withColumn("days_between_purchase_and_approval",F.datediff(F.col("order_approved_at"),F.col("order_purchase_timestamp"))))
+
+
+orders_limited = orders.withColumn("days_between_purchase_and_approval",F.datediff(F.col("order_approved_at"),F.col("order_purchase_timestamp"))).limit(10)
+
+st.dataframe(orders_limited)
+
+
+
+#df1 = df.select("customer_id", "customer_state")
+#print(df1)
+
+#n_rows = customers.count()
+
+#print(f"number od rows in this dataframe: {n_rows}")
 
