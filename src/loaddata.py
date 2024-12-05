fc5048756de8afafc228a246dff1eabb693ffe5f
Paco <pacodeoliveirasaavedra@gmail.com>
2024-12-05 12:30:39 -0300

    carregando dados pro banco de dados

--- src/loaddata.py
+++ src/loaddata.py
@@ -1,10 +1,31 @@
+import os
 import pyspark.sql.types as T         #Define os tipos nativos do PySpark
 import pyspark.sql.functions as F     #Importa as funções nativas do Spark para manipulação dos dados
 from pyspark.sql.window import Window #Importa a função utilizada para criação de janelas
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col,  to_date
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
+from dotenv import load_dotenv
 
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
 
+# Obter as variáveis do arquivo .env
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Criar a URL de conexão do banco de dados
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Criar o engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+
+####################################################################################
 #starta o spark
 spark = SparkSession.builder \
     .appName("##### Olist Data Processing #####")\
@@ -15,21 +36,51 @@ data_path = "../vendas-de-produtos-pyspark/data/olist/"
 
 # o + "nome do arquivo" é pra selecionar o arquivo dentro do path    (header = primeira linha aonde define oq é cada coisa)
 customers_df = spark.read.csv(data_path + "olist_customers_dataset.csv", header=True, inferSchema=True)
-
+cleaned_customers_df = customers_df.dropna()
 #dropa as colunas com mvalores nul
 # customers_df.dropna()
 
 
-customers_df.printSchema()
-customers_df.show(5)
-
-print("=-"* 30)
 # o + "nome do arquivo" é pra selecionar o arquivo dentro do path    (header = primeira linha aonde define oq é cada coisa)
 orders_df = spark.read.csv(data_path + "olist_orders_dataset.csv", header=True, inferSchema=True)
 orders_df = orders_df.withColumn("order_date", to_date(col("order_purchase_timestamp")))
 
-orders_df.printSchema()
-orders_df.show(5)
 
 # pra renomear só usar esse comando / nome antigo na frente , nome novo
 # customers_df = customers_df.withColumnRenamed("customer_id", "id_cliente")
+
+
+# Mostrar as primeiras linhas e esquemas para conferência
+customers_df.printSchema()
+customers_df.show(5)
+print("=-"* 30)
+
+cleaned_customers_df.printSchema()
+cleaned_customers_df.show(5)
+print("=-"* 30)
+
+orders_df.printSchema()
+orders_df.show(5)
+print("=-"* 30)
+
+# Converter os DataFrames do PySpark para pandas para salvar no PostgreSQL
+customers_pd = customers_df.toPandas()
+orders_pd = orders_df.toPandas()
+cleaned_customers_df = cleaned_customers_df.toPandas()
+
+
+try:
+    # Salvar a tabela 'customers' no banco de dados
+    customers_pd.to_sql('customers', con=engine, schema=DB_SCHEMA, if_exists='replace', index=False)
+    print("Tabela 'customers' salva com sucesso no banco de dados!")
+
+    # Salvar a tabela 'orders' no banco de dados
+    orders_pd.to_sql('orders', con=engine, schema=DB_SCHEMA, if_exists='replace', index=False)
+    print("Tabela 'orders' salva com sucesso no banco de dados!")
+
+     # Salvar a tabela 'cleaner_customers' no banco de dados
+    cleaned_customers_df.to_sql('cleaned_customers', con=engine, schema=DB_SCHEMA, if_exists='replace', index=False)
+    print("Tabela 'customers' salva com sucesso no banco de dados!")
+
+except ProgrammingError as e:
+    print(f"Erro ao salvar no banco de dados: {e}")
